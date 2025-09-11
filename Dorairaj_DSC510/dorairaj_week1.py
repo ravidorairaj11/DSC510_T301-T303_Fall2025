13fbb5768906a36ee0f076e19a5607e32d2c7a1f
ira <ravi.dorairaj@icloud.com>
2025-09-11 00:57:35 -0500

    First Python program to say Hello World demonstrate setup in PyCharm

---
+++ Dorairaj_DSC510/dorairaj_week1.py
@@ -0,0 +1,7 @@
+# Author: Ravi Dorairaj
+# Course: DSC510 - Fall 2025
+# Assignment: Week 1 - Hello World
+# Date: 09/11/2025
+# Description: First Python program to say Hello World demonstrate setup in PyCharm
+
+print("Hello World")
