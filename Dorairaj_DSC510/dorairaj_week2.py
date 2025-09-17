baf593f03400504516c45e38240cbde1692e78ae
ira <ravi.dorairaj@icloud.com>
2025-09-17 10:28:50 -0500

    Week 2 assignment - This program calculates the installation cost of fiber

---
+++ Dorairaj_DSC510/dorairaj_week2.py
@@ -0,0 +1,11 @@
+# ============================================
+# Title: Week 1 - Hello World
+# Author: Ravi Dorairaj
+# Date: 11 September 2025
+# Modified By: N/A
+# Description: This program demonstrates a basic Python setup
+#              in PyCharm by printing "Hello World" to the console.
+# ============================================
+
+# print statement to display "Hello World" in the console
+print("Hello World")
